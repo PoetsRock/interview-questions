6e5946b897dfcb7dba6469e3a8897a0be1dc9afd
ksmithut <ksmith.ut@gmail.com>
2015-02-11 14:40:02 -0700

    fixed issue with test

--- test/unique-characters-test.js
+++ test/unique-characters-test.js
@@ -7,11 +7,11 @@ describe('unique-characters.js', function () {
 
   it('should tell if a string does\'t have unique characters', function () {
     expect(unique('vivint')).to.be(false);
-    expect(unique('the quick one')).to.be(false);
+    expect(unique('the quick on')).to.be(false);
   });
 
   it('should tell if a string has unique characters', function () {
-    expect(unique('thequickone')).to.be(true);
+    expect(unique('thequickon')).to.be(true);
     expect(unique('a')).to.be(true);
     expect(unique('')).to.be(true);
   });
